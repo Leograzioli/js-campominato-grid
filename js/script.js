54943f02acdca5417c4bb1db196067f0ebe19f96
Leograzioli <leograzioli@gmail.com>
2022-10-17 21:43:21 +0200

    aggiunto function extra

--- js/script.js
+++ js/script.js
@@ -8,35 +8,40 @@ playButton.addEventListener("click", function (){
     wrapper.classList.add("active");
     const level = parseInt(levelSelectHtml.value);   
 
-    //to insert the square element with random number on the DOM and change color on click
-    for (let i = 1; i < (level + 1); i++) {
-        const element = [i];
-        const theSquare = createSquare(element);
-        
-        theSquare.addEventListener("click", onSquareClick)
-        wrapper.append(theSquare); 
-    }
+    numGenerator(level);
 })
 
 //FUNCTIONS
 
 //create a square element to add on DOM
 //number -> (number) the number to set inside of the square element
+//levelChoise -> (number) number given by the level choise
 //return -> (object) the square element
-function createSquare (number) {
-    const level = parseInt(levelSelectHtml.value);
+function createSquare (number, levelChoise) {
     const square = document.createElement("div");
     square.classList.add("square");
-    if (level === 100) {
+    if (levelChoise === 100) {
         square.classList.add("easy")
-    } else if ( level === 81) {
+    } else if ( levelChoise === 81) {
         square.classList.add("normal");
-    } else if (level === 49) {
+    } else if (levelChoise === 49) {
         square.classList.add("hard");
     }
     square.innerHTML = (number);
     return square;
 }
+
+//add square with a number inside
+//levelChoise -> (number) number given by the level choise
+function numGenerator (levelChoise) {
+    for (let i = 1; i < (levelChoise + 1); i++) {
+        const element = [i];
+        const theSquare = createSquare(element, levelChoise);
+        theSquare.addEventListener("click", onSquareClick)
+        wrapper.append(theSquare); 
+    }
+}
+
 
 //on clic add class blue to element
 function onSquareClick() {
